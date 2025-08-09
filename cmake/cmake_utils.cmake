4f37d3948379cf73199e9d6a4c229526f19c4d6c
PeterC <petercalifano.gs@gmail.com>
2025-08-09 16:27:08 +0200

    Update function to prevent failure when run on machines without CUDA

--- cmake/cmake_utils.cmake
+++ cmake/cmake_utils.cmake
@@ -78,21 +78,29 @@ function(detect_cuda_arch cuda_arch compute_cap)
             list(APPEND clean_caps "${sm}")
         endforeach()
 
-        #######
-        # BUG selection of a single device is required to avoid failure of the cmake function building the ptx. Need to fix it and allow both capabilities to be used. In principle, the code should be built for all compute capabilities but not sure how to do it now.
-        list(GET clean_caps 0 clean_caps)
-        list(GET sm_archs 0 sm_archs)
-        #######
+        if (NOT gpu_compute_caps_list)
+            message(WARNING "No CUDA device was found on this machine. Returning empty capability list.")
 
-        string(JOIN " " final_arch "${sm_archs}")
-        string(JOIN " " final_caps "${clean_caps}")
+            # Return empty
+            set(${cuda_arch} "" PARENT_SCOPE)
+            set(${compute_cap} "" PARENT_SCOPE)
 
-        set(${cuda_arch} "${final_arch}" PARENT_SCOPE)
-        set(${compute_cap} "${final_caps}" PARENT_SCOPE)
+        else()
+            #######
+            # BUG selection of a single device is required to avoid failure of the cmake function building the ptx. Need to fix it and allow both capabilities to be used. In principle, the code should be built for all compute capabilities but not sure how to do it now.
+            list(GET clean_caps 0 clean_caps)
+            list(GET sm_archs 0 sm_archs)
+            #######
 
+            string(JOIN " " final_arch "${sm_archs}")
+            string(JOIN " " final_caps "${clean_caps}")
 
-        message(STATUS "Detected CUDA compute capabilities: ${gpu_compute_caps_list}")
-        message(STATUS "Using CUDA architectures: ${final_arch}")
+            set(${cuda_arch} "${final_arch}" PARENT_SCOPE)
+            set(${compute_cap} "${final_caps}" PARENT_SCOPE)
+
+            message(STATUS "Detected CUDA compute capabilities: ${gpu_compute_caps_list}")
+            message(STATUS "Using CUDA architectures: ${final_arch}")
+        endif()
     else()
         message(WARNING "CUDA architecture detection is not supported for this platform.")
     endif()
