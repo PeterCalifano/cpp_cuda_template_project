7d114dc29c9fe30e0c281aeda404b5b898789fb1
PeterC <petercalifano.gs@gmail.com>
2025-08-09 16:49:29 +0200

    Deprecate old version and add new build_lib script (improved by GPT5)

---
+++ .deprecated/build_lib.sh
@@ -0,0 +1,229 @@
+#!/usr/bin/env bash
+# Script to build cppprojects in GNU/Linux systems
+# NOTE: this script assumes to be in the project root folder.
+# CHANGELOG: 
+# - Created January 2024,- modified May 2024 for Ubuntu 24.04 TLS by PeterC.
+# - Last updated with shell parser by PeterC, July 2024
+# - Major rework for cpp_template_project by PeterC, January 2025
+# - Improvements for cpp_cuda projects with several build types by PeterC, August 2025
+
+# Stop script if any error
+set -e 
+
+# Default values
+buildpath="build" 
+use_default_buildpath=true
+jobs=3
+rebuild_only=false
+build_type=relwithdebinfo # default build type, possible options: debug, release, relwithdebinfo, minsizerel
+add_checks=true
+CXX_FLAGS=""
+python_wrap=false
+matlab_wrap=false
+install=false
+ninja_build=false
+
+# Parse options using getopt
+# NOTE: no ":" after option means no argument, ":" means required argument, "::" means optional argument
+OPTIONS=B::,j::,i,r,t::,c,f::,p,m,n,h
+LONGOPTIONS=buildpath::,jobs::,install,rebuild_only,type-build::,checks,flagsCXX::,python-wrap,matlab-wrap,ninja-build,no-optim,help
+
+# Parsed arguments list with getopt
+PARSED=$(getopt --options ${OPTIONS} --longoptions ${LONGOPTIONS} --name "$0" -- "$@") 
+# TODO check if this is where I need to modify something to allow things like -B build, instead of -Bbuild
+
+# Check validity of input arguments 
+if [[ $? -ne 0 ]]; then
+  # e.g. $? == 1
+  #  then getopt has complained about wrong arguments to stdout
+  exit 2
+fi
+
+# Parse arguments
+eval set -- "$PARSED"
+
+# Process options (change default values if needed)
+while true; do
+  case "$1" in
+    -B|-b|--buildpath)
+      if [ -n "$2" ] && [ "$2" != "--" ]; then # Check how many args (if 2)
+        buildpath="$2"
+        use_default_buildpath=false
+        shift 2 # Shift of two args, i.e. $1 will then point to the next argument
+      else 
+      # Handle the default case (no optional argument provided), thus shift of 1
+        buildpath="build"
+        use_default_buildpath=true
+        shift
+      fi
+      ;;
+    -j|--jobs)
+      if [ -n "$2" ] && [ "$2" != "--" ]; then
+        jobs="$2"
+        shift 2
+      else
+        jobs=4
+        shift
+      fi
+      ;;
+    -r|--rebuild_only)
+      rebuild_only=true
+      shift
+      ;;
+    -t|--type-build)
+      if [ -n "$2" ] && [ "$2" != "--" ]; then
+        build_type="$2"
+        if [[ "${build_type}" == "debug" || "${build_type}" == "relwithdebinfo" || "${build_type}" == "release" ]]; then
+          CXX_FLAGS="${CXX_FLAGS} -Wall -Wextra -Wpedantic"
+        fi
+        shift 2
+      else
+        build_type=relwithdebinfo
+        CXX_FLAGS="${CXX_FLAGS} -Wall -Wextra"
+        shift
+      fi
+      ;;
+    -c|--checks)
+      add_checks=true
+      shift
+      ;;
+    -f|--flagsCXX)
+      if [ -n "$2" ] && [ "$2" != "--" ]; then
+        CXX_FLAGS="$2"
+        shift 2
+      else 
+        CXX_FLAGS="${CXX_FLAGS}"
+        shift
+      fi
+      ;;
+    -p|--python-wrap)  
+        python_wrap=true
+        shift
+      ;;
+    -m|--matlab-wrap)  
+        matlab_wrap=true
+        shift
+      ;;
+    -i|--install)
+      install=true
+      shift
+      ;;
+    -n|--no-optim)
+      no_optim=true
+      shift
+      ;;
+    -h|--help)
+      usage
+      exit 0
+      ;;
+    --)
+      shift
+      break
+      ;;
+    *)
+      echo "Not a valid option: $1" >&2
+      exit 3
+      ;;
+  esac
+done
+
+# Export path to use GCC 11.4 instead of >13.0
+#export CC=/usr/bin/gcc-11
+#export CXX=/usr/bin/g++-11
+
+# Enforce tests if build type is release
+if [ "${build_type}" == "release" ]; then
+  add_checks=true
+fi
+
+# Handle wrap requirements
+#if [ "${python_wrap}" == true ] || [ "${matlab_wrap}" == true ]; #then
+  # TODO add gtwrap repository/tools to the project or ensure they are present
+#fi
+
+if [ "${rebuild_only}" == false ]; then
+
+  # Rebuild using existing buildpath
+  echo "BUILDING project with options..."
+  echo -e "\tBuildpath: $buildpath"
+  echo -e "\tNum of jobs: $jobs"
+  echo -e "\tBuild Type: ${build_type}"
+  echo -e "\tEnforced compile flags: ${CXX_FLAGS}"
+  echo -e "\tPython wrapper build: ${python_wrap}"
+  echo -e "\tMATLAB wrapper build: ${matlab_wrap}"
+
+  sleep 0.5
+
+  BUILD_CONFIG_COMMANDS=" -DCMAKE_CXX_FLAGS=${CXX_FLAGS} \
+                          -DCMAKE_C_FLAGS=${CXX_FLAGS} \
+                          -DCMAKE_BUILD_TYPE=${build_type}"
+                          
+  # Append additional build options
+  if [ "${ninja_build}" == true ]; then
+    BUILD_CONFIG_COMMANDS+=" -GNinja"
+  fi
+
+  if [ "$python_wrap" == true ]; then
+    BUILD_CONFIG_COMMANDS+=" -DBUILD_PYTHON_WRAPPER=True"
+  fi
+
+  if [ "$matlab_wrap" == true ]; then
+    BUILD_CONFIG_COMMANDS+=" -DBUILD_MATLAB_WRAPPER=True"
+  fi
+  if [ "${no_optim}" == true ]; then
+    BUILD_CONFIG_COMMANDS+=" -DNO_OPTIMIZATION=True"
+  fi
+  #BUILD_CONFIG_COMMANDS+=""
+
+  # Generate build configuration
+    cmake -B ${buildpath} -S . ${BUILD_CONFIG_COMMANDS} 
+
+  # Stop build script if CMake configuration fails!
+  if [ $? -ne 0 ]; then
+    echo -e "\e[31mError: CMake configuration failed. Exiting build script...\e[0m"
+    exit 1
+  fi
+
+  # Build and optionally, install 
+    make -j ${jobs} -C ${buildpath} 
+
+    # Build and optionally, install 
+    make -j ${jobs} -C ${buildpath} 
+
+    if [ "${add_checks}" == true ] || [ "${install}" == true ]; then
+      if ! make test -j ${jobs} -C ${buildpath}; then
+      echo -e "\e[31mTests failed, exiting build script...\e[0m"
+      exit 1
+      fi
+    fi
+
+    if [ "${install}" == true ]; then
+      make install -j ${jobs} -C ${buildpath}
+    fi
+
+else 
+
+  # Rebuilding using existing buildpath
+  echo "REBUILDING project with options..."
+  echo -e "\tBuildpath: $buildpath"
+  echo -e "\tNum of jobs: $jobs"
+  if ! [ -d $buildpath ]; then
+      echo "ERROR: NO PREVIOUS BUILD FOUND! EXITING..." >&2
+      exit 1
+  fi
+
+    # Build and optionally, install 
+    make -j ${jobs} -C ${buildpath} 
+
+    if [ "${add_checks}" == true ] || [ "${install}" == true ]; then
+      if ! make test -j ${jobs} -C ${buildpath}; then
+      echo -e "\e[31mTests failed, exiting build script...\e[0m"
+      exit 1
+      fi
+    fi
+
+    if [ "${install}" == true ]; then
+      make install -j ${jobs} -C ${buildpath} 
+    fi
+
+fi
