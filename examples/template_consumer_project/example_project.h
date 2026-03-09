f8a0b1f098d0e64330ba98daa4bf0da632666732
PeterC <petercalifano.gs@gmail.com>
2026-03-09 17:03:27 +0100

    Update example programs to show usage of spdlogutils

--- examples/template_consumer_project/example_project.h
+++ examples/template_consumer_project/example_project.h
@@ -1,3 +1,4 @@
 #pragma once
 #include <iostream>
 #include <template_src/placeholder.h>
+#include <template_src/SpdlogUtils.h>
--- src/bin/example_program.cpp
+++ src/bin/example_program.cpp
@@ -1,7 +1,25 @@
 #include <template_src/placeholder.h>
 
+#ifndef SPDLOG_UTILS_ENABLED
+#define SPDLOG_UTILS_ENABLED 0
+#endif
+
+#if SPDLOG_UTILS_ENABLED
+#include <template_src/SpdlogUtils.h>
+#endif
+
 int main()
 {
+#if SPDLOG_UTILS_ENABLED
+    if (!spdlog_utils::InitializeLogLevelFromEnvironment())
+    {
+        spdlog_utils::ConfigureDefaultLogging();
+    }
+
+    auto objLogger_ = spdlog_utils::GetLogger("example_program");
+    objLogger_->info("Running template example program.");
+#endif
+
     placeholder::placeholder_fcn();
     return 0;
 }
