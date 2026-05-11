6a7f4567c6070c3515800f22d45c7c2f6726dba2
PeterC <petercalifano.gs@gmail.com>
2026-05-11 10:11:01 +0200

    Update tests and cmake to test wrap upgrades

--- src/wrap_interface.i
+++ src/wrap_interface.i
@@ -5,14 +5,19 @@
 namespace cpp_playground
 {
 
-#include <wrapped_impl/CWrapperPlaceholder.h>
 #include <utils/wrap_adapters/GtsamAliases.h>
+#include <wrapped_impl/CWrapperPlaceholder.h>
 
     class CWrapperPlaceholder
     {
         CWrapperPlaceholder();
         double getDataMember() const;
         void setDataMember(const double value);
+        string getTextData() const;
+        void setTextDataByConstRef(const string &charValue);
+        void setTextDataByValue(string charValue);
+        uint32_t echoFrameId(uint32_t ui32FrameId) const;
+        void printToStdout() const;
         static double multiplyBy2(const double value);
     };
 
--- src/wrapped_impl/CWrapperPlaceholder.cpp
+++ src/wrapped_impl/CWrapperPlaceholder.cpp
@@ -1,21 +1,49 @@
 #include "CWrapperPlaceholder.h"
 
+#include <iostream>
+#include <utility>
+
 namespace cpp_playground
 {
 
-double CWrapperPlaceholder::getDataMember() const
-{
-  return a_float_number_;
-}
+    double CWrapperPlaceholder::getDataMember() const
+    {
+        return a_float_number_;
+    }
 
-void CWrapperPlaceholder::setDataMember(double value)
-{
-  a_float_number_ = value;
-}
+    void CWrapperPlaceholder::setDataMember(double value)
+    {
+        a_float_number_ = value;
+    }
 
-double CWrapperPlaceholder::multiplyBy2(double value)
-{
-  return value * 2.0;
-}
+    std::string CWrapperPlaceholder::getTextData() const
+    {
+        return charTextData_;
+    }
+
+    void CWrapperPlaceholder::setTextDataByConstRef(const std::string &charValue)
+    {
+        charTextData_ = charValue;
+    }
+
+    void CWrapperPlaceholder::setTextDataByValue(std::string charValue)
+    {
+        charTextData_ = std::move(charValue);
+    }
+
+    std::uint32_t CWrapperPlaceholder::echoFrameId(std::uint32_t ui32FrameId) const
+    {
+        return ui32FrameId;
+    }
+
+    void CWrapperPlaceholder::printToStdout() const
+    {
+        std::cout << "CWrapperPlaceholder text=" << charTextData_ << '\n';
+    }
+
+    double CWrapperPlaceholder::multiplyBy2(double value)
+    {
+        return value * 2.0;
+    }
 
 } // namespace cpp_playground
